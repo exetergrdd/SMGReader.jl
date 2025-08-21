56b58291e25d762d7aaab0d3b1992be5b87a98a2
Nick Owens <owensnick@gmail.com>
2025-08-21 15:15:31 +0100

    added imports and exports

--- src/SMGReader.jl
+++ src/SMGReader.jl
@@ -2,13 +2,18 @@ module SMGReader
 
 export  HTSFileReader, indexfile, BamRecord,
         eachintersection, eachrecord,
-        qname, refname, cigarvec, cigarstring, qual, ispositive, validflag, seq,
-        samrecord,
+        qname, refname, cigarvec, cigarstring, qual, ispositive, validflag, seq, flag, leftposition, mappingquality, matepos, materefname, querylength, templatelength,
+        AuxField,
+        Modification, getmodbasestrand,
+        samrecord, writesamfile,
         VectorBuffer, setlength!
 
 include("buffer.jl")
 include("htslib.jl")
 include("htsrecord.jl")
+include("auxfields.jl")
+include("modifications.jl")
 include("sam.jl")
+
 
 end
