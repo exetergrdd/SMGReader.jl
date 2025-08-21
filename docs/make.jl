2611f894a8e532deb91635bb2699d85041b28002
Nick Owens <owensnick@gmail.com>
2025-08-21 15:18:33 +0100

    updated side bar of docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,7 @@
 using Documenter, SMGReader
 
-makedocs(sitename="SMGReader.jl")
+makedocs(sitename="SMGReader.jl",
+    pages =["index.md",
+    "Chromatin Stencilling" => "chromstencil.md",
+    "Direct RNA" => "directrna.md",
+    "API Reference" => "api.md"])
