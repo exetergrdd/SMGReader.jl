4d5df78ebccb5895648f233c502944734a6051ad
Nick Owens <owensnick@gmail.com>
2025-09-01 11:47:31 +0100

    addition exports and includes

--- src/SMGReader.jl
+++ src/SMGReader.jl
@@ -2,19 +2,23 @@ module SMGReader
 
 export  HTSFileReader, indexfile, BamRecord,
         eachintersection, eachrecord,
-        qname, refname, cigarvec, cigarstring, qual, ispositive, validflag, seq, flag, leftposition, mappingquality, matepos, materefname, querylength, templatelength,
-        AuxField, Modification, ModificationInfo, ModIterator,
+        qname, refname, cigarvec, cigarstring, qual, ispositive, validflag, seq, flag, leftposition, mappingquality, matepos, materefname, querylength, templatelength, haplotype, hashaplotype, rightpos,
+        AuxField, AuxMapMod, AuxMapModFire, Modification, ModificationInfo, ModIterator,
+        StencillingData, DirectRNA, DirectRNAAlignBlocks, processread!,
+        firemsps, firenucs, firegenome,
         samrecord, writesamfile,
         VectorBuffer, setlength!
 
 include("buffer.jl")
 include("htslib.jl")
 include("htsrecord.jl")
+include("alignmentmap.jl")
 include("auxfields.jl")
+include("processedread.jl")
 include("modifications.jl")
 include("sam.jl")
-include("alignmentmap.jl")
-include("processedread.jl")
+
+
 
 
 end
