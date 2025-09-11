403a564451162b43004b0a240be5b5005605aeac
Nick Owens <owensnick@gmail.com>
2025-09-11 18:43:26 +0100

    added exports

--- src/SMGReader.jl
+++ src/SMGReader.jl
@@ -3,7 +3,7 @@ module SMGReader
 export  HTSFileReader, indexfile, BamRecord,
         eachintersection, eachrecord,
         qname, refname, cigarvec, cigarstring, qual, ispositive, validflag, seq, flag, leftposition, mappingquality, matepos, materefname, querylength, templatelength, haplotype, hashaplotype, rightposition,
-        AuxField, AuxFieldIter, AuxMapMod, AuxMapModFire, Modification, ModificationInfo, ModIterator, mod_6mA, mod_5mC, mod_5hmC, mod_4mC, mod_inosine, mod_pseU,
+        AuxField, AuxFieldIter, AuxMapMod, AuxMapModFire, AuxMapModFiberTools, autodetectaux, Modification, ModificationInfo, ModIterator, mod_6mA, mod_5mC, mod_5hmC, mod_4mC, mod_inosine, mod_pseU,
         StencillingData, DirectRNA, DirectRNAAlignBlocks, processread!, genomecoords, firegenomecoords,
         firemsps, firenucs, firegenome,
         samrecord, writesamfile,
