50f3dacad00325368eaf9cb7fbf14910b42e419c
Nick Owens <owensnick@gmail.com>
2025-11-22 12:03:24 +0000

    fixed htslib api,  onebased bug, and added multi region iteration

--- src/htslib.jl
+++ src/htslib.jl
@@ -130,14 +130,13 @@ function openhts(path; idx_path=indexfile(path), bamthreads=Threads.nthreads())
     fp == C_NULL && error("Failed to open $path")
 
     ### set reader threads
+    
     @ccall libhts.hts_set_threads(fp::htsFile_p, bamthreads::Cint)::Cint
 
     hdr = @ccall libhts.sam_hdr_read(fp::htsFile_p)::sam_hdr_t_p
     hdr == C_NULL && error("Failed to read header")
-    
     if isfile(idx_path)
-        ### load default index if present
-        idx = @ccall libhts.sam_index_load(fp::htsFile_p, idx_path::Cstring)::hts_idx_t_p
+        idx = @ccall libhts.sam_index_load2(fp::htsFile_p, path::Cstring, idx_path::Cstring)::hts_idx_t_p ### works
         idx == C_NULL && error("Failed to load index for $idx_path")
     else
         idx = C_NULL
@@ -293,24 +292,27 @@ struct RegionIterator
 end
 
 
-@inline function regstring(chrom, loc)
+@inline function regstring(chrom, loc; onebased=true)
     iob = IOBuffer()
     write(iob, chrom)
     write(iob, ':')
-    write(iob, string(first(loc)))
+    start = ifelse(onebased, first(loc), first(loc) + 1)
+    write(iob, string(start))
     write(iob, '-')
     write(iob, string(last(loc)))
     String(take!(iob))
 end
 
 """
-    eachintersection(reader::HTSFileReader, chrom::AbstractString, loc::UnitRange{Int})
+    eachintersection(reader::HTSFileReader, chrom::AbstractString, loc::UnitRange{Int} ; onebased=true)
 
 Construct an iterator of `reader` over `chrom` and `loc`.
+Keyword `onebased = true` assume the interval provided is one-based
+
 """
-function eachintersection(reader::HTSFileReader, chrom::AbstractString, loc::UnitRange{Int})
+function eachintersection(reader::HTSFileReader, chrom::AbstractString, loc::UnitRange{Int}; onebased=true)
     record = allocateinitialbam(reader)
-    region = regstring(chrom, loc)
+    region = regstring(chrom, loc, onebased=onebased)
     set_region!(reader, region)
     return RegionIterator(reader, chrom, loc, record)
 end
@@ -358,3 +360,129 @@ end
     return (iter.record, nothing)
 end
 
+
+
+#### MultiRegionIterator
+
+const hts_pos_t = Int64
+
+struct hts_pair_pos_t
+    start::hts_pos_t
+    stop::hts_pos_t
+end
+ 
+
+struct hts_reglist_t
+    reg::Ptr{Cchar}
+    intervals::Ptr{hts_pair_pos_t}
+    tid::Cint
+    count::UInt32
+    min_start::hts_pos_t
+    max_stop::hts_pos_t
+end
+
+
+function constructregions(reader::HTSFileReader, chroms, locs; onebased=true)
+    tid_to_chrom = referencedict(reader)
+    chrom_to_tid = Dict(c => tid for (tid, c) in tid_to_chrom)
+    regions = Dict{Int32, Vector{hts_pair_pos_t}}()
+
+    for (c, l) in zip(chroms, locs)
+        tid = chrom_to_tid[c]
+        haskey(regions, tid) || (regions[tid] = hts_pair_pos_t[])
+        push!(regions[tid], hts_pair_pos_t(ifelse(onebased, first(l) - 1, first(l)), last(l)))
+    end
+
+    n_regions = length(regions)
+    c_region_list = Libc.malloc(sizeof(hts_reglist_t) * n_regions)
+    hts_region_list_ptr = Ptr{hts_reglist_t}(c_region_list)
+
+    sorted_tids = sort(collect(keys(regions)))
+
+    for (k, tid) in enumerate(sorted_tids)
+        intervals = regions[tid]
+        sort!(intervals, by = x -> x.start)
+        n = length(intervals)
+
+        min_start = first(intervals).start
+        max_stop  = maximum(hp -> hp.stop, intervals) ### the last interval isn't guaranteed to have the last stop position
+
+        c_intervals = Libc.malloc(sizeof(hts_pair_pos_t) * n)
+        c_intervals_ptr = Ptr{hts_pair_pos_t}(c_intervals)
+        unsafe_copyto!(c_intervals_ptr, pointer(intervals), n)
+
+        unsafe_store!(hts_region_list_ptr, hts_reglist_t(
+            C_NULL,
+            c_intervals_ptr,
+            tid,
+            UInt32(n),
+            min_start,
+            max_stop
+        ), k)
+    end
+
+    hts_region_list_ptr, n_regions
+end
+
+struct MultiRegionIterator
+    reader::HTSFileReader
+    record::BamRecord
+end
+
+"""
+    eachintersection(reader::HTSFileReader, chroms::Vector{<:AbstractString}, locs::Vector{UnitRange{Int}}; onebased=true)
+
+Construct an iterator of `reader` over multiple regions `chroms` and `locs`. Keyword `onebased=true` indicates whether locs are onebased inclusive.
+"""
+@inline function eachintersection(reader::HTSFileReader, chroms::Vector{<:AbstractString}, locs::Vector{UnitRange{Int}}; onebased=true)
+    record = allocateinitialbam(reader)
+    hts_region_list_ptr, n_regions = constructregions(reader, chroms, locs, onebased=onebased)
+    set_regions!(reader,  hts_region_list_ptr, n_regions)
+    return MultiRegionIterator(reader, record)
+end
+
+
+@inline function set_regions!(reader::HTSFileReader, regions_ptr::Ptr{hts_reglist_t}, n_regions::Int)
+    @assert reader.idx !== nothing "Index must be loaded to set region"
+    reader.iter != C_NULL &&  @ccall libhts.hts_itr_destroy(reader.iter::hts_itr_t_p)::Cvoid
+   
+    reader.iter = @ccall libhts.sam_itr_regions(reader.idx::hts_idx_t_p, reader.hdr::sam_hdr_t_p, regions_ptr::Ptr{hts_reglist_t}, UInt32(n_regions)::Cuint)::hts_itr_t_p
+    reader.iter == C_NULL && error("Failed to create iterator for region: $region")
+    reader.done = false
+end
+
+@inline Base.eltype(::Type{MultiRegionIterator}) = BamRecord
+@inline Base.IteratorSize(::MultiRegionIterator) = Base.SizeUnknown()
+@inline Base.iterate(iter::MultiRegionIterator) = iterate(iter, nothing)
+@inline function Base.iterate(iter::MultiRegionIterator, state)
+    reader = iter.reader
+    if reader.done
+        return nothing
+    end
+
+    ret = @ccall libhts.hts_itr_multi_next(reader.fp::htsFile_p, reader.iter::hts_itr_t_p, reader.record::Ptr{bam1_t_p})::Cint
+    if ret < 0
+        reader.done = true
+        return nothing
+    end
+    
+    # Wrap pointer in BamRecord
+    bam = unsafe_load(Ptr{bam1_t_layout}(reader.record))
+
+    iter.record.core = bam.core
+    iter.record.data = unsafe_wrap(Vector{UInt8}, bam.data, bam.l_data; own=false)
+
+    return (iter.record, nothing)
+end
+
+function set_cache_size!(reader::HTSFileReader, size_mb::Int)
+    # Set reference cache size (in bytes)
+    ret = @ccall libhts.hts_set_opt(
+        reader.fp::htsFile_p,
+        0::Cint,  # HTS_OPT_CACHE_SIZE
+        (size_mb * 1024 * 1024)::Cint
+    )::Cint
+    ret != 0 && error("Failed to set cache size")
+    # @ccall libhts.hts_set_opt(reader.fp::htsFile_p, 1::Cint, 1::Cint)::Cint
+    # ret != 0 && error("Failed to set cache the index")
+end
