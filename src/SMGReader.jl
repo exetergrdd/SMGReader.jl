2c9b9cd2e2b89a4665d313af445510dcd86ee887
Nick Owens <owensnick@gmail.com>
2025-12-15 11:26:48 +0000

    export of htsdatatype

--- src/SMGReader.jl
+++ src/SMGReader.jl
@@ -5,8 +5,8 @@ export  HTSFileReader, indexfile, BamRecord, nrecords, referencedict,
         qname, refname, cigarvec, cigarstring, qual, ispositive, validflag, seq, flag, leftposition, mappingquality, matepos, materefname, querylength, templatelength, haplotype, hashaplotype, rightposition,
         AuxField, AuxFieldIter, AuxMapMod, AuxMapModFire, AuxMapModFiberTools, autodetectaux, Modification, ModificationInfo, ModIterator, autodetectmods,
         mod_6mA, mod_5mC, mod_5hmC, mod_4mC, mod_inosine, mod_pseU, mod_2OmeA, mod_2OmeC, mod_2OmeG, mod_2OmeU,
-        StencillingData, DirectRNA, DirectRNAAlignBlocks, processread!, genomecoords, firegenomecoords,
-        firemsps, firenucs, firegenome,
+        StencillingData, DirectRNA, DirectRNAAlignBlocks, processread!, genomecoords, firegenomecoords, autodetecthtsdata,
+        firemsps, firenucs, firegenome, 
         samrecord, writesamfile,
         VectorBuffer, setlength!
 
