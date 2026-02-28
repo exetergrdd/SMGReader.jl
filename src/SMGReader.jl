2cdcfaf5a64e17acf0d2a136a3f5f8aad94c93fb
Nick Owens <owensnick@gmail.com>
2026-02-28 13:22:10 +0000

    added exports for polya etc.

--- src/SMGReader.jl
+++ src/SMGReader.jl
@@ -2,8 +2,9 @@ module SMGReader
 
 export  HTSFileReader, indexfile, BamRecord, nrecords, referencedict,
         eachintersection, eachrecord,
-        qname, refname, cigarvec, cigarstring, qual, ispositive, validflag, seq, flag, leftposition, mappingquality, matepos, materefname, querylength, templatelength, haplotype, hashaplotype, rightposition,
-        AuxField, AuxFieldIter, AuxMapMod, AuxMapModFire, AuxMapModFiberTools, autodetectaux, Modification, ModificationInfo, ModIterator, autodetectmods,
+        qname, refname, cigarvec, cigarstring, qual, ispositive, validflag, seq, flag, leftposition, mappingquality, matepos, materefname, querylength, templatelength,
+        AuxField, AuxFieldIter, AuxMapMod, AuxMapModPolyA, AuxMapModFire, AuxMapModFiberTools, autodetectaux, Modification, ModificationInfo, ModIterator, autodetectmods,
+        haplotype, hashaplotype, polyAtaillength, haspolyAtail, rightposition,
         mod_6mA, mod_5mC, mod_5hmC, mod_4mC, mod_inosine, mod_pseU, mod_2OmeA, mod_2OmeC, mod_2OmeG, mod_2OmeU,
         StencillingData, DirectRNA, DirectRNAAlignBlocks, processread!, genomecoords, firegenomecoords, autodetecthtsdata,
         firemsps, firenucs, firegenome, 
